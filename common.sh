2ac4fa925df9493d206f828c13826c69211d0b22
whoopsjohnnie <jgrundback@gmail.com>
2023-04-08 10:30:20 -0700

    Adding helm deployment and lifecycle scripts for managing the core helm package. With this we can deploy, redeploy and undeploy the full set of core services.

---
+++ common.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+
+# Set the k8s API
+export KUBECLUSTER="nuke-k3s"
+export KUBECONFIG="/Users/john/.kube/config.${KUBECLUSTER}"
+
+# Final domain name of GFS SAAS deployment, 
+# i.e. gfs.${HELM_DOMAIN}
+export HELM_DOMAIN="labber.io"
+
+# Helm values file in gfssaas folder
+export HELM_VALUES="values.yaml"
+
+# k8s
+export HELM_CLUSTER="${KUBECLUSTER}" # "nuke-k3s"
+export HELM_CONTEXT="${KUBECLUSTER}" # "nuke-k3s"
+export HELM_NAS_NFS_HOST="10.88.88.230" # "10.99.99.201"
+export HELM_NAS_NFS_PATH="/mnt/zall/k3s/nfs/${KUBECLUSTER}" # /mnt/zpool/botcanics/nfs/k3s
+export HELM_KUBECONFIG="/home/john/.kube/config.${KUBECLUSTER}"
+export HELM_RELEASE="labber"
+export HELM_NAMESPACE="labber"
+# export HELM_DEBUG="--debug"
+export HELM_DEBUG=""
+export THISDOMAIN="${HELM_DOMAIN}"
+export THISHOST="${HELM_RELEASE}.${THISDOMAIN}"
---
+++ undeploy_labber.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+THIS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+echo "This Dir: $THIS_DIR"
+source $THIS_DIR/common.sh
+
+echo " DELETING HELM PACKAGE: ${HELM_RELEASE} in K8S namespace: ${HELM_NAMESPACE}"
+
+kubectl create namespace ${HELM_NAMESPACE} || true
+
+# helm list --namespace ${HELM_NAMESPACE}
+
+helm delete \
+  --namespace ${HELM_NAMESPACE} ${HELM_DEBUG} \
+  ${HELM_RELEASE} || true
+
+helm uninstall \
+  ${HELM_RELEASE}-nfs-provisioner \
+--namespace ${HELM_NAMESPACE} || true
+
+# kubectl delete secret regcred --namespace=${HELM_NAMESPACE} || true
