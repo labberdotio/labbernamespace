37a56e1e63e7d0e22e722fbfb149806b4a2a4e5b
whoopsjohnnie <jgrundback@gmail.com>
2023-04-08 10:32:07 -0700

    Adding helm deployment and lifecycle scripts for managing the namespace related helm package. With this we can deploy, redeploy and undeploy the full set of namespace related services per namespace.

---
+++ undeploy_labbernamespace.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+THIS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+echo "This Dir: $THIS_DIR"
+source $THIS_DIR/common.sh
+
+export GFSNAMESPACE="buildkite"
+# export GFSNAMESPACE="snagger"
+
+export HELM_NAMESPACE_NAMESPACE="${HELM_NAMESPACE}"
+export HELM_NAMESPACE_RELEASE="${GFSNAMESPACE}-${HELM_RELEASE}"
+# export HELM_NAMESPACE_DOMAIN="${GFSNAMESPACE}.${HELM_DOMAIN}"
+export HELM_NAMESPACE_DOMAIN="${HELM_DOMAIN}"
+
+# kubectl create namespace ${HELM_NAMESPACE_NAMESPACE} || true
+
+helm delete \
+  --namespace ${HELM_NAMESPACE_NAMESPACE} ${HELM_DEBUG} \
+  ${HELM_NAMESPACE_RELEASE} || true
+
+# kubectl create namespace ${HELM_NAMESPACE_NAMESPACE} || true
