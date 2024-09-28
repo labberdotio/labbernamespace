ca7c851818e378447b67e70eac2ae1327d812338
whoopsjohnnie <jgrundback@gmail.com>
2024-09-28 11:39:30 -0700

    Breaking out setting up NFS storage provisioner.
    Breaking out setting up NFS storage provisioner into separate script
    as deploying into AWS does not rely upon an NFS storage solution.
    Instead each storage solution should be set up separately and the
    settings altered to reflect this change in storage.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-
-# set -x
+#!/usr/bin/env bash
+set -e
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -11,6 +10,9 @@ done
 THIS_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 echo "This Dir: $THIS_DIR"
 source $THIS_DIR/settings.sh
+
+cp ${THIS_DIR}/labber/${HELM_VALUES} ${THIS_DIR}/labber/values.yaml
+cp ${THIS_DIR}/labbernamespace/${HELM_VALUES} ${THIS_DIR}/labbernamespace/values.yaml
 
 # export GFSNAMESPACE="buildkite"
 # export GFSNAMESPACE="snagger"
@@ -30,8 +32,6 @@ export HELM_NAMESPACE_RELEASE="${GFSNAMESPACE}-${HELM_RELEASE}"
 export HELM_NAMESPACE_DOMAIN="${HELM_DOMAIN}"
 
 kubectl create namespace ${HELM_NAMESPACE_NAMESPACE} || true
-
-# cp ${THIS_DIR}/labbernamespace/${HELM_VALUES} ${THIS_DIR}/labbernamespace/values.yaml
 
 helm install \
   --namespace ${HELM_NAMESPACE_NAMESPACE} ${HELM_DEBUG} \
